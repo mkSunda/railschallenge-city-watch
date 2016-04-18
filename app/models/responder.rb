8c60ad080b5c7171c1830692b7f6dd77baa411a3
Mukesh <mksunda01@gmail.com>
2016-04-18 22:23:33 +0530

    validation for capacity, nme anmd type added

--- app/models/responder.rb
+++ app/models/responder.rb
@@ -1,2 +1,5 @@
 class Responder < ActiveRecord::Base
+  validates :capacity, presence: true, :inclusion => {:in => 1..5}
+  validates :name, uniqueness: { case_sensitive: false }
+  validates :type, presence: true
 end
