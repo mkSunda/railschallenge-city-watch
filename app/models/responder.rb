69c0fd50813cce73d9e3c4dda2da859288938de4
Mukesh <mksunda01@gmail.com>
2016-04-18 22:26:02 +0530

    Disable sti

--- app/models/responder.rb
+++ app/models/responder.rb
@@ -1,4 +1,7 @@
 class Responder < ActiveRecord::Base
+  # disable STI
+  self.inheritance_column = :_type_disabled
+
   validates :capacity, presence: true, :inclusion => {:in => 1..5}
   validates :name, uniqueness: { case_sensitive: false }
   validates :type, presence: true
